08fc0cc97304721ffce7f0b8806161e7feb9c589
Abdullah <120916864+mrflash5002@users.noreply.github.com>
2023-05-03 23:38:19 +0200

    table with for loops created, save button added, COLUMN_COUNT and DEFAULT_ROW_COUNT added as values

--- classes.py
+++ classes.py
@@ -16,7 +16,13 @@ class action :
         return f'id = {self.id} spent on {self.description} | amount {self.amount}€ | {self.time}'
 
 
-    def save_data(self) : # function does not work properly--> overwriting existing data in csv
+
+    def extract_data_from_table(self) :
+        pass # use this to make data readible by csv and send it to save_data as paramater to be saved to file
+
+
+
+    def save_data(self, data) : # function does not work properly--> overwriting existing data in csv
         with open('data.csv', 'w', newline='') as file :
             writer= csv.writer(file)
             writer.writerow([self.id, self.description, self.amount, self.time]) # use pandas learn it
@@ -24,9 +30,6 @@ class action :
 
 
 
-x = action(1, 'netto', 25.63)
-
-action.save_data(x)
 
 
 
@@ -38,6 +41,12 @@ class cell :
 
 
     def create_cell(self, location) :
-        entry = tkinter.Entry(location)
+        entry = tkinter.Entry(location, font=('Arial',15,'bold'))
 
         self.cell_object = entry
+
+
+    @staticmethod
+    def save_button(location) :
+        btn = tkinter.Button(location, text='save')
+        return btn
--- main.py
+++ main.py
@@ -1,16 +1,28 @@
 from tkinter import *
 from classes import cell
-from values import CELL_HEIGHT, CELL_WIDTH
+from values import CELL_HEIGHT, CELL_WIDTH, COLUMN_COUNT, DEFAULT_ROW_COUNT
 
 root = Tk()
 root.state('zoomed')
 root.title('MONEY TRACKING')
 
+my_x = 20
+my_y = 50
 
-mycell = cell(20, 50)
 
-cell.create_cell(mycell, root)
-mycell.cell_object.place(x = mycell.x, y = mycell.y, width = CELL_WIDTH, height = CELL_HEIGHT)
+for _ in range(DEFAULT_ROW_COUNT) : # add new line button to add extra rows
+    for _ in range(COLUMN_COUNT) :
+        mycell = cell(my_x, my_y)
 
+        cell.create_cell(mycell, root)
+        mycell.cell_object.place(x = mycell.x, y = mycell.y, width = CELL_WIDTH, height = CELL_HEIGHT)
+        my_x += CELL_WIDTH
+    my_y += CELL_HEIGHT
+    my_x = 20
+
+
+
+save_button = cell.save_button(root)
+save_button.place(x = 400, y = 600, width=50, height=25)
 
 root.mainloop()
